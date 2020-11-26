fff6e5fba0e65561e0db32792636356d77c2874e
Maxime Jouard <maximejouard@MacBook-Air-de-Maxime.local>
2020-11-26 12:09:26 +0100

    Version 2.0

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+const taxList = {"DE":20, "UK": 21, "FR": 20, "IT": 25, "ES": 19, "PL": 21, "RO": 20, "NL": 20, "BE": 24, "EL": 20, "CZ": 19, "PT": 23, "HU": 27, "SE": 23, "AT": 22, "BG": 21, "DK": 21, "FI": 17, "SK": 18,"IE": 21, "HR":23 ,"LT":23 ,"SI":24 ,"LV":20 ,"EE":22 ,"CY":21 ,"LU":25 ,"MT":20}
 
 
 // ==================================
@@ -17,6 +18,7 @@ const paramChecker = (req, res, next) => {
 	const bill = {
     prices: req.body.prices,
     quantities: req.body.quantities,
+		country: req.body.country
   }
 	for (let attr in bill) {
 		if (bill[attr] === undefined)
@@ -25,6 +27,9 @@ const paramChecker = (req, res, next) => {
   if (bill.prices.length != bill.quantities.length) {
     return res.status(400).json({ "error" : "prices and quantities have not the same length for /bill" })
   }
+	if (!taxList[bill.country]){
+		return res.status(400).json({ "error" : "this country does not exist" })
+	}
 	next()
 }
 
@@ -57,11 +62,13 @@ app.post('/bill', paramChecker, (req, res) => {
   const bill = {
     prices: req.body.prices,
     quantities: req.body.quantities,
+		country: req.body.country
   }
   let result = 0;
   for (let i = 0; i < bill.prices.length; i++){
     result += bill.prices[i] * bill.quantities[i];
   }
+	result = result * (1 + taxList[bill.country]/100)
   res.send({"total":result});
 })
 // curl -H "Content-Type:application/json" -X POST -d '{"prices":[10,20], "quantities":[1,2]}' http://localhost:3000/bill
